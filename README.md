41d5d88df735b53b60fbfb3b13e955e8f1e51350
Yummy Fran Palomares <yumpalomares@gmail.com>
2025-08-19 00:06:41 +0800

    screenshots

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# React + Vite
+## Screenshots
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+![Output](./public/Screenshot_18-8-2025_235354_localhost.jpeg)
 
-Currently, two official plugins are available:
+![Console](./public/Screenshot%202025-08-18%20235758.png)
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,23 +1,38 @@
-import '../styles/product-card.css'
+import "../styles/product-card.css";
 
-const ProductCard = ({ image, name, price, description, product: { slug } }) => {
+const ProductCard = ({
+  image,
+  name,
+  price,
+  description,
+  product: { slug },
+}) => {
   return (
     <div className="product-card">
-        <div className="product-image">
-            <img src={image} alt={name} />
+      <div className="product-image">
+        <img src={image} alt={name} />
+      </div>
+      <div className="product-details">
+        <div className="title">
+          <div className="name">{name}</div>
+          <div className="description">{description}</div>
         </div>
-        <div className="product-details">
-            <div className="title">
-                <div className="name">{name}</div>
-                <div className="description">{description}</div>
-            </div>
-            <div className="cta">
-                <div className="price">&#8369; {price}</div>
-                <button>Buy</button>
-            </div>
+        <div className="cta">
+          <div className="price">&#8369; {price}</div>
+          <button
+            onClick={() => {
+              console.log(
+                `%cBought ${name}`,
+                "background:#111; color:#0ff; padding:4px 8px; border-radius:6px; font-weight:bold;"
+              );
+            }}
+          >
+            Buy
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
